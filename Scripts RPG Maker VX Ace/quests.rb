63902011bb95dbd38ed03c8774e6b9f5984d3e23
Duplessier Bastien <bastien.duplessier@gmail.com>
2014-08-19 22:40:45 +0200

    First quests

---
+++ Scripts RPG Maker VX Ace/quests.rb
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# Quest : L'éveil des monts
+Quest.create(id: 1, label: :fight, name: "L'éveil du mont", desc: "Le volcan de Roda est de nouveau en activité. Vous pourriez aller étudier ce phénomène? (Ah oui, faut aimer la chaleur!).", cost: 1600, gold: 11400, item: [2, 3], cancellable: false)
+Quest.create(id: 2, label: :fight, name: "Le sablier d'or", desc: "Un brigand vend des copies de notre célèbre "Sablier d'Or". Faut l'arrêter ! Le faussaire se trouve dans le désert Jeraw.", cost: 2400, gold: 18000, weapons: [5], cancellable: false)
