9b39f260c83434ed20efa95aa80964108ce8137c
Duplessier Bastien <bastien.duplessier@gmail.com>
2014-08-24 01:20:57 +0200

    typo3

--- Scripts RPG Maker VX Ace/quests.rb
+++ Scripts RPG Maker VX Ace/quests.rb
@@ -11,6 +11,6 @@ Quest.create(id: 2, label: :fight, rank: 2, name: "Le sablier d'or",
   cost: 2400, gold: 18000, weapons: [5], cancellable: false)
 # Quest : Etoffe magique
 Quest.create(id: 3, label: :dispatch, rank: 1, name: "Etoffe magique", 
-  desc: "Bonjour, j'échange du tissu magik contre du conton magik. Vous en avez?", 
+  desc: "Bonjour, j'échange du tissu magik contre du coton magik. Vous en avez?", 
   cost: 1400, items: [5], req_items: [4], reco_job: [1], duration: 100, 
   duration_type: :steps, cancellable: false)
