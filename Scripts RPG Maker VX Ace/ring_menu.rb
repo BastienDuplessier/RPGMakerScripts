51d810ea9d1468b728f05dababc6080d3cd3860b
Duplessier Bastien <bastien.duplessier@gmail.com>
2014-10-13 18:42:22 +0200

    Change SChar to CharacterIcon

--- Scripts RPG Maker VX Ace/ring_menu.rb
+++ Scripts RPG Maker VX Ace/ring_menu.rb
@@ -103,7 +103,7 @@ module Zangther
   end
 
 
-  class Game_SChar < Game_Character
+  class Game_CharacterIcon < Game_Character
     def initialize
       super
       @move_speed = 1
@@ -843,7 +843,7 @@ module Zangther
     private
     def create_command_crescent
       icons = $game_party.members.map do |actor|
-        char = Game_SChar.new
+        char = Game_CharacterIcon.new
         char.set_graphic(actor.character_name,actor.character_index)
         Zangther::Sprite_Character_Icon.new(@viewport, char)
       end
